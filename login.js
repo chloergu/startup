9eee04ffa0f5cc48b668c96fd84ad7a7ab7d4fec
Chloe Guerrero <chloergu@gmail.com>
2023-05-27 16:15:50 -0600

    added login.js, fixed image sizing

---
+++ login.js
@@ -0,0 +1,11 @@
+
+
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    localStorage.setItem()
+
+    const nameEl = document.querySelector("#name");
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "play.html";
+  }
