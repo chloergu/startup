b858d624e3ae10f68ec3d716d97b2312d4037f29
Chloe Guerrero <chloergu@gmail.com>
2023-05-27 18:23:45 -0600

    changes to login javascript

--- login.js
+++ login.js
@@ -1,11 +1,30 @@
+//Store user's username and password
+function store()
+{
+    var user = document.getElementById('username');
+    var pass = document.getElementById('password');
+    localStorage.setItem('username', user.value)
+    localStorage.setItem('password', pass.value)
 
+    window.location.href = "home.html";
+}
 
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    localStorage.setItem()
+//login if username and password match storage
+function checkLogin()
+{
+    //get set stored data
+    var userStored = localStorage.getItem('user')
+    var passStored = localStorage.getItem('pass')
 
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
-  }
+    //data entered from login-form
+    var userName = document.getElementById('userName');
+    var userPass = document.getElementById('userPass');
+
+    if (userStored == userName && passStored == userPass) {
+        window.location.href = "home.html";
+    }
+    else {
+        alert("Please enter a valid username and password.");
+    }
+
+}
